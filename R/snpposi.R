3cd3b076cddc958dbcc0e409f11a678f6fb4cea0
jombart <t.jombart@imperial.ac.uk>
2013-12-23 12:04:39 +0700

    Added snpposi.plot and its doc.

--- R/snpposi.R
+++ R/snpposi.R
@@ -55,3 +55,56 @@ snpposi.test.DNAbin <- function(x, ...){
     return(out)
 } # end snpposi.test.DNAbin
 
+
+
+
+
+
+
+
+
+
+
+################
+## snpposi.plot
+################
+
+## GENERIC
+snpposi.plot <- function(...){
+    UseMethod("snpposi.plot")
+}
+
+
+
+
+## METHOD FOR INTEGER - BASIC METHOD
+snpposi.plot.integer <- function(x, genome.size, smooth=0.1, col="royalblue", alpha=.2, ...){
+    ## make ggplot output ##
+    out <- ggplot(data.frame(x=x), aes(x=x))
+    out <- out + geom_density(adjust=smooth, fill=transp(col,alpha=alpha), colour=col) + geom_rug(colour=col,alpha=.7)
+    out <- out + labs(x="Nucleotide position", title="Distribution of SNPs in the genome")
+
+    ## return ##
+    return(out)
+} # end snpposi.plot.integer
+
+
+
+
+
+## METHOD FOR NUMERIC
+snpposi.plot.numeric <- function(x, ...){
+    out <- snpposi.plot(as.integer(x), ...)
+    return(out)
+}
+
+
+
+
+## METHOD FOR DNABIN
+snpposi.plot.DNAbin <- function(x, ...){
+    out <- snpposi.plot(x=as.integer(seg.sites(x)),
+                        genome.size=ncol(x), ...)
+    return(out)
+} # end snpposi.plot.DNAbin
+
