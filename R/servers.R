e5440596cff443f7df60fedb5cec379ac925dc1b
Thibaut Jombart <tjombart@imperial.ac.uk>
2014-03-11 16:23:37 +0000

    Added doc for servers; added new function adegenetServer

--- R/servers.R
+++ R/servers.R
@@ -1,3 +1,25 @@
+
+
+##################
+## adegenetServer
+##################
+## function starting a web-server
+## for adegenet tools
+adegenetServer <- function(what=c("DAPC")){
+    what <- tolower(what)
+    what <- match.arg(what)
+    if(what=="dapc"){
+        .dapcServer()
+    }
+
+    return(invisible())
+}
+
+
+###############
+## .dapcServer
+###############
+## hidden function - DAPC server
 .dapcServer <- function(){
     runApp(system.file("dapcServer",package="adegenet"))
 }
