2c0a30780896a696aaac68c1b355d40f45b7fa5b
romunov <romunov@gmail.com>
2015-02-25 21:21:17 +0100

    fixed @ mistranslations in dist.genpop.R

--- R/dist.genpop.R
+++ R/dist.genpop.R
@@ -127,7 +127,7 @@
 #' obj <- as.genpop(microsatt$tab)
 #' 
 #' listDist <- lapply(1:5, function(i) cailliez(dist.genpop(obj,met=i)))
-#' for(i in 1:5) {attr(listDist[[i]],"Labels") <- obj@pop.names}
+#' for(i in 1:5) {attr(listDist[[i]],"Labels") <- obj@@pop.names}
 #' listPco <- lapply(listDist, dudi.pco,scannf=FALSE)
 #' 
 #' par(mfrow=c(2,3))
