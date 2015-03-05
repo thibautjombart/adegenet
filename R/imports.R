a2b4802780a84a4b04d3eca103494599f1060e72
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-03-04 21:18:23 -0800

    roxygen2 modified NAMESPACE: imports and DynLib
    I added these roxygen comments into a new file called R/imports.R
    I have also imported all of the ggplot2 functions in the case that
    we might want to expand the plotting capabilities.

---
+++ R/imports.R
@@ -0,0 +1,10 @@
+#' @useDynLib adegenet
+#' 
+#' @importFrom utils packageDescription
+#' @importFrom stats kmeans optimize
+#' @importFrom MASS lda
+#' @importFrom ape as.character.DNAbin as.DNAbin as.DNAbin.alignment as.DNAbin.character as.DNAbin.list as.list.DNAbin as.matrix.DNAbin cbind.DNAbin c.DNAbin "[.DNAbin" labels.DNAbin print.DNAbin rbind.DNAbin dist.dna
+#' @importFrom igraph graph.data.frame V "V<-" E "E<-" layout.fruchterman.reingold as.igraph plot.igraph print.igraph graph.adjacency clusters
+#' @importFrom shiny runApp renderPrint
+#' @import methods ade4 ggplot2
+NULL
