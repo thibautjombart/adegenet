724a60c7db04d2e1961859ef08adead960b778a4
Thibaut Jombart <thibautjombart@gmail.com>
2015-03-19 09:41:19 -0400

    adding constructor for genind, now using initialize and roxy2 doc

---
+++ R/constructors.R
@@ -0,0 +1,223 @@
+
+#' genind constructor
+#'
+#' The function \code{new} has a method for building \linkS4class{genind} objects.
+#' See the class description of \linkS4class{genind} for more information on this data structure.
+#' The functions \code{genind} and \code{as.genind} are aliases for \code{new("genind", ...)}.
+#'
+#' Most users do not need using the constructor, but merely to convert raw allele data using \code{\link{df2genind}} and related functions.
+#'
+#' @export
+#' @docType methods
+#'
+#' @aliases initialize,genind-methods
+#' @aliases genind
+#' @aliases as.genind
+#'
+#' @rdname new.genind
+#'
+#' @param .Object prototyped object (generated automatically when calling 'new')
+#' @param tab A matrix of integers corresponding to the @tab slot of a genind object, with individuals in rows and alleles in columns, and containing either allele counts (if type="codom") or allele presence/absence (if type="PA")
+#' @param pop an optional factor with one value per row in \code{tab} indicating the population of each individual
+#' @param prevcall an optional call to be stored in the object
+#' @param ploidy an integer vector indicating the ploidy of the individual; each individual can have a different value; if only one value is provided, it is recycled to generate a vector of the right length.
+#' @param type a character string indicating the type of marker: codominant ("codom") or presence/absence ("PA")
+#' @param ... further arguments passed to other methods (currently not used)
+#'
+#' @return a \linkS4class{genind} object
+setMethod("initialize", "genind", function(.Object, tab, pop=NULL, prevcall=NULL, ploidy=2L, type=c("codom","PA"), ...){
+   ## HANDLE ARGUMENTS ##
+    out <- .Object
+    if(is.null(colnames(tab))) stop("tab columns have no name.")
+    if(is.null(rownames(tab))) {rownames(tab) <- 1:nrow(tab)}
+
+    ## force matrix & integer
+    old.rownames <- rownames(tab)
+    old.colnames <- colnames(tab)
+    old.dim <- dim(tab)
+    if(typeof(tab)!="integer"){
+        tab <- as.integer(tab)
+        dim(tab) <- old.dim
+        rownames(tab) <- old.rownames
+        colnames(tab) <- old.colnames
+    }
+    type <- match.arg(type)
+    nind <- nrow(tab)
+    ploidy <- as.integer(ploidy)
+    ploidy <- rep(ploidy, length=nind)
+
+
+    ## HANDLE LABELS ##
+    ## loc names is not type-dependent
+    temp <- gsub("[.][^.]*$", "", old.colnames)
+    temp <- .rmspaces(temp)
+    loc.names <- unique(temp)
+    nloc <- length(loc.names)
+
+    ## ind names is not type-dependent either
+    ## only use generic label if no name or duplicates
+    if(is.null(rownames(tab))) {
+        rownames(tab) <- .genlab("", nind)
+    }
+    ind.names <- rownames(tab)
+    if(length(unique(ind.names))!=length(ind.names)) {
+        warning("duplicate labels detected for some individuals; using generic labels")
+        rownames(tab) <- ind.names <- .genlab("", nind)
+    }
+
+    if(type=="codom"){
+        ## loc.nall
+        loc.nall <-  table(temp)[match(loc.names,names(table(temp)))]
+        loc.nall <- as.integer(loc.nall)
+        names(loc.nall) <- loc.names
+
+        ## loc.fac
+        loc.fac <- rep(loc.names,loc.nall)
+
+        ## alleles name
+        temp <- colnames(tab)
+        temp <- gsub("^.*[.]","",temp)
+        temp <- .rmspaces(temp)
+        all.names <- split(temp,loc.fac)
+        all.names <- all.names[loc.names]
+        loc.fac <- as.factor(loc.fac)
+
+    } else { # end if type=="codom" <=> if type=="PA"
+        loc.fac <- NULL
+        all.names <- NULL
+        loc.nall <- NULL
+    }
+
+    ## Ideally I should use an 'initialize' method here
+    out <- new("genind")
+    out@tab <- tab
+    out@ind.names <- ind.names
+    out@loc.names <- loc.names
+    out@loc.nall <- loc.nall
+    out@loc.fac <- loc.fac
+    out@all.names <- all.names
+
+    ## populations name (optional)
+    ## beware, keep levels of pop sorted in
+    ## there order of appearance
+    if(!is.null(pop)) {
+        # convert pop to a factor if it is not
+        if(!is.factor(pop)) {pop <- factor(pop)}
+        out@pop <- pop
+        out@pop.names <- levels(pop)
+    }
+
+    ## ploidy
+    plo <- as.integer(ploidy)
+    if(any(plo < 1L)) stop("ploidy inferior to 1")
+    out@ploidy <- plo
+
+    ## type of marker
+    out@type <- as.character(type)
+
+    if(is.null(prevcall)) {prevcall <- match.call()}
+    out@call <- prevcall
+
+    return(out)
+})
+
+as.genind <- genind <- function(...){
+    out <- new("genind", ...)
+    return(out)
+} # end genind
+
+
+
+
+##################
+# Function genpop
+##################
+genpop <- function(tab,prevcall=NULL,ploidy=as.integer(2),type=c("codom","PA")){
+
+    ## HANDLE ARGS ##
+    if(is.null(colnames(tab))) stop("tab columns have no name.")
+    if(is.null(rownames(tab))) {rownames(tab) <- 1:nrow(tab)}
+
+    ## force matrix & integer
+    old.rownames <- rownames(tab)
+    old.colnames <- colnames(tab)
+    old.dim <- dim(tab)
+    if(typeof(tab)!="integer"){
+        tab <- as.integer(tab)
+        dim(tab) <- old.dim
+        rownames(tab) <- old.rownames
+        colnames(tab) <- old.colnames
+    }
+    type <- match.arg(type)
+    ploidy <- as.integer(ploidy)
+    npop <- nrow(tab)
+
+
+    ## HANDLE LABELS ##
+
+    ## loc names is not type-dependent
+    temp <- gsub("[.][^.]*$", "", old.colnames)
+    temp <- .rmspaces(temp)
+    loc.names <- unique(temp)
+    nloc <- length(loc.names)
+
+    ## pop names is not type-dependent either
+    ## only use generic label if no name or duplicates
+    if(is.null(rownames(tab))) {
+        rownames(tab) <- .genlab("", npop)
+    }
+    pop.names <- rownames(tab)
+    if(length(unique(pop.names))!=length(pop.names)) {
+        warning("duplicate labels detected for some populations; using generic labels")
+        rownames(tab) <- pop.names <- .genlab("", npop)
+    }
+
+    if(type=="codom"){
+        ## loc.nall
+        loc.nall <-  table(temp)[match(loc.names,names(table(temp)))]
+        loc.nall <- as.integer(loc.nall)
+        names(loc.nall) <- loc.names
+
+        ## loc.fac
+        loc.fac <- rep(loc.names,loc.nall)
+
+        ## alleles name
+        temp <- colnames(tab)
+        temp <- gsub("^.*[.]","",temp)
+        temp <- .rmspaces(temp)
+        all.names <- split(temp,loc.fac)
+        all.names <- all.names[loc.names]
+        loc.fac <- as.factor(loc.fac)
+
+    } else { # end if type=="codom" <=> if type=="PA"
+        loc.fac <- NULL
+        all.names <- NULL
+        loc.nall <- NULL
+    }
+
+    ## build final output
+    res <- new("genpop")
+
+    res@tab <- tab
+    res@pop.names <- pop.names
+    res@loc.names <- loc.names
+    res@loc.nall <- loc.nall
+    res@loc.fac <- loc.fac
+    res@all.names <- all.names
+    res@ploidy <- ploidy
+    res@type <- as.character(type)
+
+    if(is.null(prevcall)) {prevcall <- match.call()}
+    res@call <- prevcall
+
+    return(res)
+
+} # end genpop
+
+
+
+######################
+# alias for as.genpop
+######################
+as.genpop <- genpop
+
