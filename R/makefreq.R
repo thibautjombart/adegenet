531a749b669e0a3544ac79b819f4c34b015d127f
Thibaut Jombart <thibautjombart@gmail.com>
2015-03-18 20:05:39 -0400

    fixed makefreq; added roxy doc; now is a S4 generic + methods

--- R/makefreq.R
+++ R/makefreq.R
@@ -1,11 +1,91 @@
+
 ####################
 # Function makefreq
 ####################
-makefreq <- function(x,quiet=FALSE,missing=NA,truenames=TRUE){
 
-  if(!is.genpop(x)) stop("x is not a valid genpop object")
-  ##if(x@type=="PA") stop("frequencies not computable for presence/asbence data")
-  checkType(x)
+#' Compute allelic frequencies
+#'
+#' The function \code{makefreq} is a generic to compute allele frequencies.
+#' These can be derived for \linkS4class{genind} or \linkS4class{genpop} objects.
+#' In the case of \linkS4class{genind} objects, data are kept at the individual level, but standardised so that allele frequencies sum up to 1.
+#'
+#' There are 3 treatments for missing values: \cr - NA: kept as NA.\cr - 0:
+#' missing values are considered as zero. Recommended for a PCA on
+#' compositionnal data.\cr - "mean": missing values are given the mean
+#' frequency of the corresponding allele. Recommended for a centred PCA.
+#'
+#' @param x a \linkS4class{genind} or \linkS4class{genpop} object.
+#' @param quiet logical stating whether a conversion message must be printed
+#' (TRUE,default) or not (FALSE).
+#' @param missing treatment for missing values. Can be NA, 0 or "mean" (see
+#' details)
+#' @param truenames a logical indicating whether true labels (as opposed to
+#' generic labels) should be used to name the output.
+#' @param ... further arguments (curently unused)
+#'
+#' @return Returns a list with the following components: \item{tab}{matrix of
+#' allelic frequencies (rows: populations; columns: alleles).}
+#' \item{nobs}{number of observations (i.e. alleles) for each population x
+#' locus combinaison.} \item{call}{the matched call}
+#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
+#' @seealso \code{\link{genpop}}
+#' @keywords manip multivariate
+#'
+#'#' @examples
+#'
+#' \dontrun{
+#' data(microbov)
+#' obj1 <- microbov
+#'
+#' obj2 <- genind2genpop(obj1)
+#'
+#' Xfreq <- makefreq(obj2,missing="mean")
+#'
+#'
+#' # perform a correspondance analysis on counts data
+#'
+#' Xcount <- genind2genpop(obj1,missing="chi2")
+#' ca1 <- dudi.coa(as.data.frame(Xcount@@tab),scannf=FALSE)
+#' s.label(ca1$li,sub="Correspondance Analysis",csub=1.2)
+#' add.scatter.eig(ca1$eig,nf=2,xax=1,yax=2,posi="topleft")
+#'
+#' # perform a principal component analysis on frequency data
+#' pca1 <- dudi.pca(Xfreq$tab,scale=FALSE,scannf=FALSE)
+#' s.label(pca1$li,sub="Principal Component Analysis",csub=1.2)
+#' add.scatter.eig(pca1$eig,nf=2,xax=1,yax=2,posi="top")
+#' }
+#'
+#' @export makefreq
+#' @docType methods
+#' @rdname makefreq
+#'
+setGeneric("makefreq", function(x, ...) standardGeneric("makefreq"))
+
+#' @export
+#' @rdname makefreq
+#' @aliases makefreq,genind-methods
+#' @aliases makefreq.genind
+setMethod ("makefreq", signature(x="genind"), function(x, quiet=FALSE, missing=NA, truenames=TRUE, ...){
+  if(!quiet) cat("\n Finding allelic frequencies from a genpop object... \n")
+
+  res <- list()
+
+  out <- x@tab / x@ploidy
+
+  if(!quiet) cat("\n...done.\n\n")
+
+  return(out)
+}) #end makefreq for genind
+
+
+
+
+## genpop method
+#' @export
+#' @rdname makefreq
+#' @aliases makefreq,genpop-methods
+#' @aliases makefreq.genpop
+setMethod ("makefreq", signature(x="genpop"), function(x, quiet=FALSE, missing=NA, truenames=TRUE, ...){
 
   if(!quiet) cat("\n Finding allelic frequencies from a genpop object... \n")
 
@@ -55,5 +135,5 @@ makefreq <- function(x,quiet=FALSE,missing=NA,truenames=TRUE){
   }
 
   return(res)
-} #end makefreq
+}) #end makefreq for genpop
 
