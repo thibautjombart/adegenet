183b1604a6156b5444ebf835572d9cd5518344de
Thibaut Jombart <thibautjombart@gmail.com>
2015-03-19 13:48:47 -0400

    commented out useless code in fstat stuff

--- R/fstat.R
+++ R/fstat.R
@@ -8,24 +8,24 @@
 fstat <- function(x, pop=NULL, fstonly=FALSE){
     message("Sorry, this function depends on hierfstat, and has been moved to the package hierfstat.")
     return()
-    ## cat("\nSorry, hierfstat package has been disabled - this function will be restored in a future release.\n")
-    ## return(invisible())
-    ## misc checks
-    if(!is.genind(x)) stop("x is not a valid genind object")
-    ## if(!require(hierfstat)) stop("hierfstat package is required. Please install it.")
-    if(x@ploidy != as.integer(2)) stop("not implemented for non-diploid genotypes")
-    checkType(x)
+    ## ## cat("\nSorry, hierfstat package has been disabled - this function will be restored in a future release.\n")
+    ## ## return(invisible())
+    ## ## misc checks
+    ## if(!is.genind(x)) stop("x is not a valid genind object")
+    ## ## if(!require(hierfstat)) stop("hierfstat package is required. Please install it.")
+    ## if(x@ploidy != as.integer(2)) stop("not implemented for non-diploid genotypes")
+    ## checkType(x)
 
-    if(is.null(pop)) pop <- x@pop
-    if(is.null(pop)) stop("no pop factor provided")
-    if(length(pop)!=nrow(x@tab)) stop("pop has a wrong length.")
+    ## if(is.null(pop)) pop <- x@pop
+    ## if(is.null(pop)) stop("no pop factor provided")
+    ## if(length(pop)!=nrow(x@tab)) stop("pop has a wrong length.")
 
-    ## computations
-    dat <- genind2hierfstat(x)[,-1]
-    res <- varcomp.glob(levels=data.frame(pop), loci=dat)$F
+    ## ## computations
+    ## dat <- genind2hierfstat(x)[,-1]
+    ## res <- varcomp.glob(levels=data.frame(pop), loci=dat)$F
 
-    if(fstonly) {res <- res[1,1]}
-    return(res)
+    ## if(fstonly) {res <- res[1,1]}
+    ## return(res)
 }
 
 
