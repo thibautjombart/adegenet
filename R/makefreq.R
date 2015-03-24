c6456d3e8ba2dde0716992fbb8db2fd9456c5b79
Thibaut Jombart <thibautjombart@gmail.com>
2015-03-24 14:56:26 +0000

    fixing example of makefreq

--- R/makefreq.R
+++ R/makefreq.R
@@ -35,21 +35,17 @@
 #' \dontrun{
 #' data(microbov)
 #' obj1 <- microbov
-#'
 #' obj2 <- genind2genpop(obj1)
 #'
-#' Xfreq <- makefreq(obj2,missing="mean")
-#'
-#'
 #' # perform a correspondance analysis on counts data
-#'
-#' Xcount <- genind2genpop(obj1,missing="chi2")
-#' ca1 <- dudi.coa(as.data.frame(Xcount@@tab),scannf=FALSE)
+#' Xcount <- tab(obj2, NA.method="zero")
+#' ca1 <- dudi.coa(Xcount,scannf=FALSE)
 #' s.label(ca1$li,sub="Correspondance Analysis",csub=1.2)
 #' add.scatter.eig(ca1$eig,nf=2,xax=1,yax=2,posi="topleft")
 #'
 #' # perform a principal component analysis on frequency data
-#' pca1 <- dudi.pca(Xfreq$tab,scale=FALSE,scannf=FALSE)
+#' Xfreq <- makefreq(obj2,missing="mean")
+#' pca1 <- dudi.pca(Xfreq,scale=FALSE,scannf=FALSE)
 #' s.label(pca1$li,sub="Principal Component Analysis",csub=1.2)
 #' add.scatter.eig(pca1$eig,nf=2,xax=1,yax=2,posi="top")
 #' }
