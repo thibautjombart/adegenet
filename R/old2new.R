596ec3eab6846083439f5c1f50efab9e00b22bc7
Thibaut Jombart <thibautjombart@gmail.com>
2015-06-22 15:00:34 +0100

    renaming slots in data objects

--- R/old2new.R
+++ R/old2new.R
@@ -1,24 +1,24 @@
 ##################
 # Methods old2new
 ##################
-#' Convert objects with obsolete classes into new objects 
-#'  
-#' The genind and genlight objects have changed in Adegenet version 2.0. They 
-#' have each gained strata and hierarchy slots. What's more is that the genind 
+#' Convert objects with obsolete classes into new objects
+#'
+#' The genind and genlight objects have changed in Adegenet version 2.0. They
+#' have each gained strata and hierarchy slots. What's more is that the genind
 #' objects have been optimized for storage and now store the tab slot as
 #' integers instead of numerics. This function will convert old genind or
 #' genlight objects to new ones seamlessly.
-#'  
-#' @name old2new 
+#'
+#' @name old2new
 #' @rdname old2new
-#' @aliases old2new old2new,ANY-method old2new-methods old2new,genind-method 
+#' @aliases old2new old2new,ANY-method old2new-methods old2new,genind-method
 #' old2new,genpop-method old2new,genlight-method
-#' @docType methods 
+#' @docType methods
 #' @param object a genind or genlight object from version 1.4 or earlier.
-#' 
+#'
 #' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}\cr
 #' Zhian N. Kamvar \email{kamvarz@@science.oregonstate.edu}
-#' @keywords methods classes manip 
+#' @keywords methods classes manip
 #' @export
 old2new <- function(object) standardGeneric("old2new")
 
@@ -29,34 +29,34 @@ setMethod("old2new", "genind", function(object){
   object@hierarchy <- NULL
   object@strata    <- NULL
   if (object@type == "codom"){
-    
+
     if (!is.integer(object@tab) && length(object@ploidy == 1)){
       xtab   <- object@tab
       newtab <- as.integer(object@tab * object@ploidy)
       object@tab <- matrix(newtab, nrow = nrow(xtab), ncol = ncol(xtab),
                            dimnames = dimnames(xtab))
-      object@ploidy <- rep(object@ploidy, nrow(object@tab))  
+      object@ploidy <- rep(object@ploidy, nrow(object@tab))
     }
-    
+
     names(object@loc.names) <- NULL
     rownames(object@tab)    <- object@ind.names
     names(object@all.names) <- object@loc.names
     object@all.names     <- lapply(object@all.names, setNames, NULL)
-    colnames(object@tab) <- unlist(lapply(object@loc.names, 
-                                          function(i) paste(i, object@all.names[[i]], 
-                                                            sep = ".")), 
+    colnames(object@tab) <- unlist(lapply(object@loc.names,
+                                          function(i) paste(i, object@all.names[[i]],
+                                                            sep = ".")),
                                    use.names = FALSE)
     levels(object@loc.fac) <- object@loc.names
     names(object@loc.n.all) <- object@loc.names
   }
-  
+
   if (!is.null(object@pop)){
     names(object@pop.names) <- NULL
     levels(object@pop) <- object@pop.names
   }
   object@call <- match.call()
   return(object)
-  
+
 #   res@tab <- as.matrix(x$tab)
 #   res@ind.names <- as.character(x$ind.names)
 #   res@loc.names <- as.character(x$loc.names)
@@ -74,7 +74,7 @@ setMethod("old2new", "genind", function(object){
 #   res@call <- match.call()
 #   res@ploidy <- as.integer(2)
 #   res@type <- "codom"
-# 
+#
 #   if(length(object) > theoLength) warning("optional content else than pop and pop.names was not converted")
 
   # return(res)
@@ -87,14 +87,14 @@ setMethod("old2new", "genlight", function(object){
 })
 
 setMethod("old2new", "genpop", function(object){
-  
+
   ## names(object@pop.names) <- NULL
   ## names(object@loc.names) <- NULL
-  
+
   rownames(object@tab) <- object@pop.names
-  colnames(object@tab) <- unlist(lapply(object@loc.names, 
-                                        function(i) paste(i, object@all.names[[i]], 
-                                                          sep = ".")), 
+  colnames(object@tab) <- unlist(lapply(object@loc.names,
+                                        function(i) paste(i, object@all.names[[i]],
+                                                          sep = ".")),
                                  use.names = FALSE)
   names(object@all.names) <- object@loc.names
   object@all.names     <- lapply(object@all.names, setNames, NULL)
@@ -102,7 +102,7 @@ setMethod("old2new", "genpop", function(object){
   return(object)
   #   x <- object
 #   res <- new("genpop")
-# 
+#
 #   res@tab <- as.matrix(x$tab)
 #   res@pop.names <- as.character(x$pop.names)
 #   res@loc.names <- as.character(x$loc.names)
@@ -111,10 +111,10 @@ setMethod("old2new", "genpop", function(object){
 #   res@all.names <- as.list(x$all.names)
 #   res@ploidy <- as.integer(2)
 #   res@type <- "codom"
-# 
+#
 #   res@call <- match.call()
-# 
+#
 #   if(length(object)>7) warning("optional content was not converted")
-# 
+#
 #   return(res)
 })
--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname){
     pkg.version <- packageDescription("adegenet", fields = "Version")
 
-    startup.txt <- paste("\n   /// adegenet ", pkg.version, " is loaded",
-                         "\n   > overview: '?adegenet'",
+    startup.txt <- paste("\n   /// adegenet ", pkg.version, " is loaded //////////",
+                         "\n\n   > overview: '?adegenet'",
                          "\n   > tutorials/doc/questions: 'adegenetWeb()' ",
                          "\n   > bug reports/feature resquests: adegenetIssues()\n\n", sep="")
 
