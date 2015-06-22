af4f0561c32f8528c417e1761225e477fc30c62f
Thibaut Jombart <thibautjombart@gmail.com>
2015-06-22 19:27:47 +0100

    progress on tutorial basics

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname){
     pkg.version <- packageDescription("adegenet", fields = "Version")
 
-    startup.txt <- paste("\n   /// adegenet ", pkg.version, " is loaded //////////",
+    startup.txt <- paste("\n   /// adegenet ", pkg.version, " is loaded ///////////////",
                          "\n\n   > overview: '?adegenet'",
                          "\n   > tutorials/doc/questions: 'adegenetWeb()' ",
                          "\n   > bug reports/feature resquests: adegenetIssues()\n\n", sep="")
