19418b0436cc9b6ff3096bb1043e787744283f99
Thibaut Jombart <thibautjombart@gmail.com>
2015-07-10 13:24:42 +0100

    new Hs function, much faster

--- R/Hs.R
+++ R/Hs.R
@@ -1,10 +1,43 @@
-############################
-# Hs (expected heterozygosity)
-############################
-Hs <- function(x, truenames=TRUE) {
+#'
+#' Expected heterozygosity (Hs)
+#'
+#' This function computes the expected heterozygosity (Hs) within
+#' populations of a \linkS4class{genpop} object. This function is
+#' available for codominant markers (\code{@type="codom"}) only. Hs is
+#' commonly used for measuring within population genetic diversity (and
+#' as such, it still has sense when computed from haploid data).
+#'
+#' @aliases Hs
+#' @aliases Hs.test
+#'
+#' @export
+#'
+#' @seealso \code{\link{spca}}
+#' @details
+#' Let \emph{m(k)} be the number of alleles of locus \emph{k}, with a
+#' total of \emph{K} loci. We note \eqn{f_i} the allele frequency of
+#' allele \emph{i} in a given population. Then, \eqn{Hs} is given for a
+#' given population by:\cr
+#'
+#' \eqn{\frac{1}{K} \sum_{k=1}^K (1 - \sum_{i=1}^{m(k)} f_i^2)}
+#'
+#' @author Thibaut Jombart \email{t.jombart@@imperial.ac.uk}
+#'
+#' @param x a \linkS4class{genind} or \linkS4class{genpop} object.
+#' @param pop only used if x is a \linkS4class{genind}; an optional factor to be used as population; if not provided, pop(x) is used.
+#'
+#' @return a vector of Hs values (one value per population)
+#' @examples
+#' \dontrun{
+#' data(nancycats)
+#' Hs(genind2genpop(nancycats))
+#' }
+#'
+Hs <- function(x, pop=NULL) {
 
     ## CHECKS
     if(is.genind(x)){
+        if(!is.null(pop)) pop(x) <- pop
         x <- genind2genpop(x, quiet=TRUE)
     }
     if(!is.genpop(x)) stop("x is not a valid genpop object")
@@ -12,11 +45,18 @@ Hs <- function(x, truenames=TRUE) {
 
 
     ## MAIN COMPUTATIONS
-    x.byloc <- seploc(x)
+    ## get number of typed loci per pop
+    if(any(is.na(tab(x)))){
+        temp <- apply(tab(x),1,tapply, locFac(x), function(e) !any(is.na(e)))
+        nLoc.typed <- apply(temp,2,sum)
+    } else {
+        nLoc.typed <- nLoc(x)
+    }
 
-    lX <- lapply(x.byloc, function(e) makefreq(e, quiet=TRUE))
-    lres <- lapply(lX, function(X) 1- apply(X^2,1,sum))
-    res <- apply(as.matrix(data.frame(lres)),1,mean)
+    F <- tab(x, freq=TRUE, NA.method="asis")
+    res <- 1-(apply(F^2,1,sum, na.rm=TRUE))/nLoc.typed
 
     return(res)
 } # end Hs
+
+
