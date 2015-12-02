832412e1c11c9b9ccbc1b932b287edbd2268d392
Thibaut <thibautjombart@gmail.com>
2015-12-02 15:15:53 -0500

    fixed typo

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
     startup.txt <- paste("\n   /// adegenet ", pkg.version, " is loaded ////////////",
                          "\n\n   > overview: '?adegenet'",
                          "\n   > tutorials/doc/questions: 'adegenetWeb()' ",
-                         "\n   > bug reports/feature resquests: adegenetIssues()\n\n", sep="")
+                         "\n   > bug reports/feature requests: adegenetIssues()\n\n", sep="")
 
     packageStartupMessage(startup.txt)
 }
