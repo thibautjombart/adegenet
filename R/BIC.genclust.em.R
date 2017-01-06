12b06458b1e022de65bf61d1a586e7df2dba461f
Thibaut Jombart <thibautjombart@gmail.com>
2017-01-06 11:39:33 +0000

    adding BIC for genclust

---
+++ R/BIC.genclust.em.R
@@ -0,0 +1,24 @@
+#' Compute Bayesian Information Criterion (BIC) for genclust.em
+#'
+#' Do not use. We work on that stuff. Contact us if interested.
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @export
+#'
+#' @param object An object returned by the function \code{\link{genclust.em}}.
+#'
+#' @param ... Further arguments for compatibility with the \code{BIC} generic
+#'     (currently not used).
+#' 
+#' @seealso  \code{\link{genclust.em}} to generate clustering solutions.
+#'
+#' 
+BIC.genclust.em <- function(object, ...) {
+
+    ## The number of parameters is defined as the number of clusters
+    n <- length(object$group)
+    k <- length(levels(object$group))
+    -2 * object$ll + log(n) * k
+}
+
