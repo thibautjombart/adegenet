81136b69f7168b5d3c36e2390e0a6fe0dca9607d
Thibaut Jombart <thibautjombart@gmail.com>
2017-05-08 16:32:54 +0100

    adding doc for export_to_webapp; examples for DAPC and sPCA. Passing check.

--- R/export_to_webapp.R
+++ R/export_to_webapp.R
@@ -5,9 +5,6 @@
 #'
 #' @param x The analysis to be exported.
 #'
-#' @param pop A factor indicating the population of each individual, in the same
-#'   order as in the analysis.
-#'
 #' @param info A data.frame with additional information containing at least the
 #'   following columns: \code{key} (individual label), \code{latitude}, and
 #'   \code{longitude}.
@@ -17,6 +14,7 @@
 #' @export
 #'
 #' @rdname export_to_webapp
+#'
 
 export_to_webapp <- function(x, ...) {
   UseMethod("export_to_webapp")
@@ -45,6 +43,19 @@ export_to_webapp.default <- function(x, ...) {
 
 #' @export
 #' @rdname export_to_webapp
+#' @examples
+#'
+#' data(sim2pop)
+#'
+#' dapc1 <- dapc(sim2pop, n.pca = 10, n.da = 1)
+#'
+#' info <- data.frame(key = indNames(sim2pop),
+#'                    lat = other(sim2pop)$xy[,2],
+#'                    lon = other(sim2pop)$xy[,1],
+#'                    Population = pop(sim2pop))
+#'
+#' out <- export_to_webapp(dapc1, info)
+#' head(out)
 
 export_to_webapp.dapc <- function(x, info, ...) {
 
@@ -65,7 +76,76 @@ export_to_webapp.dapc <- function(x, info, ...) {
                                support)
 
   ## process 'info' (checks that required columns are there)
-  info <- check_info(info, key)
+  info <- .check_info(info, key)
+
+  out <- merge(analysis, info, by = "key")
+  return(out)
+}
+
+
+
+
+
+
+#' @export
+#' @rdname export_to_webapp
+
+export_to_webapp.dudi <- function(x, info, ...) {
+
+  ## Extract principal components, groups, assigned groups and the corresponding
+  ## probability.
+
+  pcs <- x$li
+  colnames(pcs) <- paste0("PC", 1:ncol(pcs))
+  key <- rownames(pcs)
+
+  analysis <- cbind.data.frame(key, pcs)
+
+  ## process 'info' (checks that required columns are there)
+  info <- .check_info(info, key)
+
+  out <- merge(analysis, info, by = "key")
+  return(out)
+}
+
+
+
+
+
+
+#' @export
+#' @rdname export_to_webapp
+#' @examples
+#'
+#' data(rupica)
+#'
+#' spca1 <- spca(rupica, type=5, d1 = 0, d2 = 2300,
+#'               plot = FALSE, scannf = FALSE,
+#'               nfposi = 2,nfnega = 0)
+#'
+#' info <- data.frame(key = indNames(rupica),
+#'                    lat = rupica$other$xy[,2],
+#'                    lon = rupica$other$xy[,1])
+#'
+#' out <- export_to_webapp(spca1, info)
+#' head(out)
+#'
+
+export_to_webapp.spca <- function(x, info, ...) {
+
+  ## Extract principal components, groups, assigned groups and the corresponding
+  ## probability.
+
+  pcs <- x$li
+  colnames(pcs) <- paste0("PC", 1:ncol(pcs))
+  lag_pcs <- x$ls
+  colnames(lag_pcs) <- paste0("Lag_PC", 1:ncol(pcs))
+  key <- rownames(pcs)
+
+  analysis <- cbind.data.frame(key, pcs, lag_pcs)
+
+  ## process 'info' (checks that required columns are there)
+  info <- .check_info(info, key)
 
   out <- merge(analysis, info, by = "key")
   return(out)
@@ -79,19 +159,18 @@ export_to_webapp.dapc <- function(x, info, ...) {
 ## This internal function will merely check the content of the extra 'info'
 ## being provided, making sure key, latitude and longitude are provided.
 
-check_info <- function(info, ref_keys) {
+.check_info <- function(info, ref_keys,
+                       look_for = c("key", "lat", "lon")) {
+
   info <- as.data.frame(info)
 
-  if (!"key" %in% names(info)) {
-    stop("'info' is missing a 'key' column")
-  }
-
-  if (!"lat" %in% names(info)) {
-    stop("'info' is missing a 'lat' column")
-  }
-
-  if (!"lon" %in% names(info)) {
-    stop("'info' is missing a 'lon' column")
+  if (length(look_for) > 0L) {
+    for (e in look_for) {
+      if (!e %in% names(info)) {
+        msg <- sprintf("'info' is missing a '%s' column", e)
+        stop(msg)
+      }
+    }
   }
 
   nb_missing <- sum(!ref_keys %in% info$key)
