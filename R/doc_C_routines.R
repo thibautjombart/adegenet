8ec26cd26234b2f55b2bdceadc70cb54c5dca807
thibautjombart <thibautjombart@gmail.com>
2017-08-18 12:12:14 +0100

    adding doc file for internal routines - was now causing issues

---
+++ R/doc_C_routines.R
@@ -0,0 +1,15 @@
+#' Internal C routines
+#'
+#' These functions are internal C routines used in adegenet. Do not use them
+#' unless you know what you are doing.
+#'
+#' @aliases CheckAllSeg GLdotProd GLsumFreq GLsumInt binIntToBytes bytesToBinInt
+#' bytesToInt nb_shared_all
+#'
+#' @author Thibaut Jombart
+#'
+#' @export
+#'
+#' @rdname doc_C_routines
+#' 
+".internal_C_routines" <- NULL
