9a28c1e28eb45d433fffddd73856588263d5999c
Thibaut Jombart <thibautjombart@gmail.com>
2018-01-31 14:48:53 +0000

    moving data to appropriate place; updated doc

--- R/export_to_mvmapper.R
+++ R/export_to_mvmapper.R
@@ -1,3 +1,4 @@
+
 #' Export analysis for mvmapper visualisation
 #'
 #' \code{mvmapper} is an interactive tool for visualising outputs of a
@@ -105,22 +106,36 @@ export_to_mvmapper.default <- function(x, ...) {
 #' @export
 #' @rdname export_to_mvmapper
 #' @examples
-#' 
-#' # An example using the microsatellite dataset of Dupuis et al. 2016 (781 individuals, 10 loci, doi: 10.1111/jeb.12931)
+#'
+#' # An example using the microsatellite dataset of Dupuis et al. 2016 (781
+#' # individuals, 10 loci, doi: 10.1111/jeb.12931)
+#'
 #' # Reading input file from adegenet
+#'
 #' input_data <- system.file("data/swallowtails.rda", package="adegenet")
 #' data(swallowtails)
-#' 
+#'
+#'
 #' # conducting a DAPC (n.pca determined using xvalDapc, see ??xvalDapc)
-#' dapc1 <- dapc(butterflies, n.pca=40, n.da=200)
-#' 
-#' # read in swallowtails_loc.csv, which contains "key", "lat", and "lon" columns with column headers (this example contains additional columns containing species identifications, locality descriptions, and COI haplotype clades)
-#' input_locs <- system.file("data/swallowtails_loc.csv", package="adegenet")
-#' loc <- read.csv(input_locs, header=T)
-#' 
-#' # generate mvmapper input file, automatically write the output to a csv, and name the output csv "mvMapper_Data.csv" 
-#' out <- export_to_mvmapper(dapc1, loc, write_file=TRUE, out_file="mvMapper_Data.csv")
-#' 
+#'
+#' dapc1 <- dapc(swallowtails, n.pca=40, n.da=200)
+#'
+#'
+#' # read in swallowtails_loc.csv, which contains "key", "lat", and "lon"
+#' # columns with column headers (this example contains additional columns
+#' # containing species identifications, locality descriptions, and COI
+#' # haplotype clades)
+#'
+#' input_locs <- system.file("files/swallowtails_loc.csv", package = "adegenet")
+#' loc <- read.csv(input_locs, header = TRUE)
+#'
+#'
+#' # generate mvmapper input file, automatically write the output to a csv, and
+#' # name the output csv "mvMapper_Data.csv"
+#'
+#' out <- export_to_mvmapper(dapc1, loc, write_file = TRUE,
+#'                           out_file = "mvMapper_Data.csv")
+#'
 
 export_to_mvmapper.dapc <- function(x, info, write_file = TRUE, out_file = NULL, ...) {
 
