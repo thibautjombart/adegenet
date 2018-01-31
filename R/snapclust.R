bbab23644f2f001287836b3a435aa448c8063c68
Thibaut Jombart <thibautjombart@gmail.com>
2018-01-31 17:31:23 +0000

    fix for numerical prcision problem

--- R/snapclust.R
+++ R/snapclust.R
@@ -133,177 +133,180 @@
 #' }
 
 snapclust <- function(x, k, pop.ini = "ward", max.iter = 100, n.start = 10,
-                        n.start.kmeans = 50,
-                        hybrids = FALSE, dim.ini = 100,
-                        hybrid.coef = NULL, parent.lab = c('A', 'B'), ...) {
-    ## This function uses the EM algorithm to find ML group assignment of a set
-    ## of genotypes stored in a genind object into 'k' clusters. We need an
-    ## initial cluster definition to start with. The rest of the algorithm
-    ## consists of:
+                      n.start.kmeans = 50, hybrids = FALSE, dim.ini = 100,
+                      hybrid.coef = NULL, parent.lab = c('A', 'B'), ...) {
+  ## This function uses the EM algorithm to find ML group assignment of a set
+  ## of genotypes stored in a genind object into 'k' clusters. We need an
+  ## initial cluster definition to start with. The rest of the algorithm
+  ## consists of:
 
-    ## i) compute the matrix of allele frequencies
+  ## i) compute the matrix of allele frequencies
 
-    ## ii) compute the likelihood of each genotype for each group
+  ## ii) compute the likelihood of each genotype for each group
 
-    ## iii) assign genotypes to the group for which they have the highest
-    ## likelihood
+  ## iii) assign genotypes to the group for which they have the highest
+  ## likelihood
 
-    ## iv) go back to i) until convergence
+  ## iv) go back to i) until convergence
 
 
-    ## Disable multiple starts if the initial condition is not random
-    use.random.start <- is.null(pop.ini)
-    if (!use.random.start) {
-        n.start <- 1L
+  ## Disable multiple starts if the initial condition is not random
+  use.random.start <- is.null(pop.ini)
+  if (!use.random.start) {
+    n.start <- 1L
+  }
+
+  if (n.start < 1L) {
+    stop(sprintf(
+      "n.start is less than 1 (%d); using n.start=1", n.start))
+  }
+
+  if (hybrids && k > 2) {
+    warning(sprintf(
+      "forcing k=2 for hybrid mode (requested k is %d)", k))
+    k <- 2
+  }
+
+
+  ## Handle hybrid coefficients; these values reflect the contribution of the
+  ## first parental population to the allele frequencies of the hybrid
+  ## group. For instance, a value of 0.75 indicates that 'a' contributes to
+  ## 75%, and 'b' 25% of the allele frequencies of the hybrid - a typical
+  ## backcross F1 / a.
+
+  if (hybrids) {
+    if (is.null(hybrid.coef)) {
+      hybrid.coef <- 0.5
+    }
+    hybrid.coef <- .tidy.hybrid.coef(hybrid.coef)
+  }
+
+
+  ## Initialisation using 'find.clusters'
+  if (!is.null(pop.ini)) {
+    if (tolower(pop.ini)[1] %in% c("kmeans", "k-means")) {
+      pop.ini <- find.clusters(x, n.clust = k, n.pca = dim.ini,
+                               n.start = n.start.kmeans,
+                               method = "kmeans", ...)$grp
+    } else if (tolower(pop.ini)[1] %in% c("ward")) {
+      pop.ini <- find.clusters(x, n.clust = k, n.pca = dim.ini,
+                               method = "ward", ...)$grp
+    }
+  }
+
+  ## There is one run of the EM algo for each of the n.start random initial
+  ## conditions.
+  ll <- -Inf # this will be the total loglike
+
+  for (i in seq_len(n.start)) {
+
+    ## Set initial conditions: if initial pop is NULL, we create a random
+    ## group definition (each clusters have same probability)
+    if (use.random.start) {
+      pop.ini <- sample(seq_len(k), nInd(x), replace=TRUE)
     }
 
-    if (n.start < 1L) {
-        stop(sprintf(
-            "n.start is less than 1 (%d); using n.start=1", n.start))
+    ## process initial population, store levels
+    pop.ini <- factor(pop.ini)
+    lev.ini <- levels(pop.ini)[1:k] # k+1 would be hybrids
+
+    ## ensure 'pop.ini' matches 'k'
+    if (! (length(levels(pop.ini)) %in% c(k, k + length(hybrid.coef))) ) {
+      stop("pop.ini does not have k clusters")
     }
 
-    if (hybrids && k > 2) {
-        warning(sprintf(
-            "forcing k=2 for hybrid mode (requested k is %d)", k))
-        k <- 2
+    ## initialisation
+    group <- factor(as.integer(pop.ini)) # set levels to 1:k (or k+1)
+    genotypes <- tab(x)
+    n.loc <- nLoc(x)
+    counter <- 0L
+    converged <- FALSE
+
+
+    ## This is the actual EM algorithm
+
+    while(!converged && counter<=max.iter) {
+
+      ## get table of allele frequencies (columns) by population (rows);
+      ## these are stored as 'pop.freq'; note that it will include extra
+      ## rows for different types of hybrids too.
+
+      if (hybrids) {
+        pop(x) <- group
+        id.parents <- .find.parents(x)
+        x.parents <- x[id.parents]
+        pop.freq <- tab(genind2genpop(x.parents, quiet=TRUE),
+                        freq=TRUE)
+        pop.freq <- rbind(pop.freq, # parents
+                          .find.freq.hyb(pop.freq, hybrid.coef)) # hybrids
+      } else {
+        pop.freq <- tab(genind2genpop(x, pop=group, quiet=TRUE),
+                        freq=TRUE)
+      }
+
+      ## ensures no allele frequency is exactly zero
+      pop.freq <- .tidy.pop.freq(pop.freq, locFac(x))
+
+      ## get likelihoods of genotypes in every pop
+      ll.mat <- apply(genotypes, 1, .ll.genotype, pop.freq, n.loc)
+
+      ## assign individuals to most likely cluster
+      previous.group <- group
+      group <- apply(ll.mat, 2, which.max)
+
+      ## check convergence
+      ## converged <- all(group == previous.group)
+      old.ll <- .global.ll(previous.group, ll.mat)
+      new.ll <- .global.ll(group, ll.mat)
+      if (!is.finite(new.ll)) {
+        ## stop(sprintf("log-likelihood at iteration %d is not finite (%f)",
+        ##              counter, new.ll))
+      }
+      converged <- abs(old.ll - new.ll) < 1e-14
+      counter <- counter + 1L
+
     }
 
+    ## ## store the best run so far
+    ## new.ll <- .global.ll(group, ll.mat)
 
-    ## Handle hybrid coefficients; these values reflect the contribution of the
-    ## first parental population to the allele frequencies of the hybrid
-    ## group. For instance, a value of 0.75 indicates that 'a' contributes to
-    ## 75%, and 'b' 25% of the allele frequencies of the hybrid - a typical
-    ## backcross F1 / a.
+    if (new.ll > ll || i == 1L) {
+      ## store results
+      ll <- new.ll
+      out <- list(group = group, ll = ll)
 
-    if (hybrids) {
-        if (is.null(hybrid.coef)) {
-            hybrid.coef <- 0.5
-        }
-        hybrid.coef <- .tidy.hybrid.coef(hybrid.coef)
+      browser()
+
+      ## group membership probability
+      rescaled.ll.mat <- .rescale.ll.mat(ll.mat)
+      out$proba <- prop.table(t(exp(rescaled.ll.mat)), 1)
+
+      out$converged <- converged
+      out$n.iter <- counter
     }
+  } # end of the for loop
 
-
-    ## Initialisation using 'find.clusters'
-    if (!is.null(pop.ini)) {
-        if (tolower(pop.ini)[1] %in% c("kmeans", "k-means")) {
-            pop.ini <- find.clusters(x, n.clust = k, n.pca = dim.ini,
-                                     n.start = n.start.kmeans,
-                                     method = "kmeans", ...)$grp
-        } else if (tolower(pop.ini)[1] %in% c("ward")) {
-            pop.ini <- find.clusters(x, n.clust = k, n.pca = dim.ini,
-                                     method = "ward", ...)$grp
-        }
+  ## restore labels of groups
+  out$group <- factor(out$group)
+  if (hybrids) {
+    if (!is.null(parent.lab)) {
+      lev.ini <- parent.lab
     }
-
-    ## There is one run of the EM algo for each of the n.start random initial
-    ## conditions.
-    ll <- -Inf # this will be the total loglike
-
-    for (i in seq_len(n.start)) {
-
-        ## Set initial conditions: if initial pop is NULL, we create a random
-        ## group definition (each clusters have same probability)
-        if (use.random.start) {
-            pop.ini <- sample(seq_len(k), nInd(x), replace=TRUE)
-        }
-
-        ## process initial population, store levels
-        pop.ini <- factor(pop.ini)
-        lev.ini <- levels(pop.ini)[1:k] # k+1 would be hybrids
-
-        ## ensure 'pop.ini' matches 'k'
-        if (! (length(levels(pop.ini)) %in% c(k, k + length(hybrid.coef))) ) {
-            stop("pop.ini does not have k clusters")
-        }
-
-        ## initialisation
-        group <- factor(as.integer(pop.ini)) # set levels to 1:k (or k+1)
-        genotypes <- tab(x)
-        n.loc <- nLoc(x)
-        counter <- 0L
-        converged <- FALSE
+    hybrid.labels <- paste0(hybrid.coef, "_", lev.ini[1], "-",
+                            1 - hybrid.coef, "_", lev.ini[2])
+    lev.ini <- c(lev.ini, hybrid.labels)
+  }
+  levels(out$group) <- lev.ini
+  colnames(out$proba) <- lev.ini
 
 
-        ## This is the actual EM algorithm
+  ## compute the number of parameters; it is defined as the number of 'free'
+  ## allele frequencies, multiplied by the number of groups
 
-        while(!converged && counter<=max.iter) {
+  out$n.param <- (ncol(genotypes) - n.loc) * length(lev.ini)
 
-            ## get table of allele frequencies (columns) by population (rows);
-            ## these are stored as 'pop.freq'; note that it will include extra
-            ## rows for different types of hybrids too.
-
-            if (hybrids) {
-                pop(x) <- group
-                id.parents <- .find.parents(x)
-                x.parents <- x[id.parents]
-                pop.freq <- tab(genind2genpop(x.parents, quiet=TRUE),
-                                freq=TRUE)
-                pop.freq <- rbind(pop.freq, # parents
-                                  .find.freq.hyb(pop.freq, hybrid.coef)) # hybrids
-            } else {
-                pop.freq <- tab(genind2genpop(x, pop=group, quiet=TRUE),
-                                freq=TRUE)
-            }
-
-            ## ensures no allele frequency is exactly zero
-            pop.freq <- .tidy.pop.freq(pop.freq, locFac(x))
-
-            ## get likelihoods of genotypes in every pop
-            ll.mat <- apply(genotypes, 1, .ll.genotype, pop.freq, n.loc)
-
-            ## assign individuals to most likely cluster
-            previous.group <- group
-            group <- apply(ll.mat, 2, which.max)
-
-            ## check convergence
-            ## converged <- all(group == previous.group)
-            old.ll <- .global.ll(previous.group, ll.mat)
-            new.ll <- .global.ll(group, ll.mat)
-            if (!is.finite(new.ll)) {
-                ## stop(sprintf("log-likelihood at iteration %d is not finite (%f)",
-                ##              counter, new.ll))
-            }
-            converged <- abs(old.ll - new.ll) < 1e-14
-            counter <- counter + 1L
-
-        }
-
-        ## ## store the best run so far
-        ## new.ll <- .global.ll(group, ll.mat)
-
-        if (new.ll > ll || i == 1L) {
-            ## store results
-            ll <- new.ll
-            out <- list(group = group, ll = ll)
-
-            ## group membership probability
-            out$proba <- prop.table(t(exp(ll.mat)), 1)
-            out$converged <- converged
-            out$n.iter <- counter
-        }
-    } # end of the for loop
-
-    ## restore labels of groups
-    out$group <- factor(out$group)
-    if (hybrids) {
-        if (!is.null(parent.lab)) {
-            lev.ini <- parent.lab
-        }
-        hybrid.labels <- paste0(hybrid.coef, "_", lev.ini[1], "-",
-                               1 - hybrid.coef, "_", lev.ini[2])
-        lev.ini <- c(lev.ini, hybrid.labels)
-    }
-    levels(out$group) <- lev.ini
-    colnames(out$proba) <- lev.ini
-
-
-    ## compute the number of parameters; it is defined as the number of 'free'
-    ## allele frequencies, multiplied by the number of groups
-
-    out$n.param <- (ncol(genotypes) - n.loc) * length(lev.ini)
-
-    class(out) <- c("snapclust", "list")
-    return(out)
+  class(out) <- c("snapclust", "list")
+  return(out)
 }
 
 
@@ -325,23 +328,23 @@ snapclust <- function(x, k, pop.ini = "ward", max.iter = 100, n.start = 10,
 ## for haploids.
 
 .ll.genotype <- function(x, pop.freq, n.loc){
-    ## homozygote (diploid)
-    ## p(AA) = f(A)^2 for each locus
-    ll.homoz.one.indiv <- function(f) {
-        sum(log(f[x == 2L]), na.rm = TRUE) * 2
-    }
+  ## homozygote (diploid)
+  ## p(AA) = f(A)^2 for each locus
+  ll.homoz.one.indiv <- function(f) {
+    sum(log(f[x == 2L]), na.rm = TRUE) * 2
+  }
 
-    ll.homoz <- apply(pop.freq, 1, ll.homoz.one.indiv)
+  ll.homoz <- apply(pop.freq, 1, ll.homoz.one.indiv)
 
-    ## heterozygote (diploid, expl with 2 loci)
-    ## p(Aa)p(Bb) = 2^n.loc * f(A)f(a) f(B)f(b)
-    ll.hetero.one.indiv <- function(f) {
-        sum(log(f[x == 1L]), na.rm = TRUE) + n.loc * log(2)
-    }
+  ## heterozygote (diploid, expl with 2 loci)
+  ## p(Aa)p(Bb) = 2^n.loc * f(A)f(a) f(B)f(b)
+  ll.hetero.one.indiv <- function(f) {
+    sum(log(f[x == 1L]), na.rm = TRUE) + n.loc * log(2)
+  }
 
-    ll.heteroz <- apply(pop.freq, 1, ll.hetero.one.indiv)
+  ll.heteroz <- apply(pop.freq, 1, ll.hetero.one.indiv)
 
-    return(ll.homoz + ll.heteroz)
+  return(ll.homoz + ll.heteroz)
 }
 
 
@@ -353,7 +356,7 @@ snapclust <- function(x, k, pop.ini = "ward", max.iter = 100, n.start = 10,
 ## assignments and a table of ll of genotypes in each group
 
 .global.ll <- function(group, ll){
-    sum(t(ll)[cbind(seq_along(group), as.integer(group))], na.rm=TRUE)
+  sum(t(ll)[cbind(seq_along(group), as.integer(group))], na.rm=TRUE)
 }
 
 
@@ -372,9 +375,9 @@ snapclust <- function(x, k, pop.ini = "ward", max.iter = 100, n.start = 10,
 ## proximity to the first parental population.
 
 .tidy.hybrid.coef <- function(w) {
-    w <- w[w > 0 & w < 1]
-    w <- sort(unique(round(c(w, 1-w), 4)), decreasing = TRUE)
-    w
+  w <- w[w > 0 & w < 1]
+  w <- sort(unique(round(c(w, 1-w), 4)), decreasing = TRUE)
+  w
 }
 
 
@@ -402,9 +405,9 @@ snapclust <- function(x, k, pop.ini = "ward", max.iter = 100, n.start = 10,
 ## alleles in columns.
 
 .find.freq.hyb <- function(x, w) {
-    out <- cbind(w, 1-w) %*% x
-    rownames(out) <- w
-    out
+  out <- cbind(w, 1-w) %*% x
+  rownames(out) <- w
+  out
 }
 
 
@@ -417,11 +420,11 @@ snapclust <- function(x, k, pop.ini = "ward", max.iter = 100, n.start = 10,
 ## from the parental populations.
 
 .find.parents <- function(x) {
-    ## matrix of pairwise distances between clusters, using Nei's distance
-    D <- as.matrix(dist.genpop(genind2genpop(x, quiet = TRUE), method = 1))
-    parents <- which(abs(max(D)-D) < 1e-14, TRUE)[1,]
-    out <- which(as.integer(pop(x)) %in% parents)
-    out
+  ## matrix of pairwise distances between clusters, using Nei's distance
+  D <- as.matrix(dist.genpop(genind2genpop(x, quiet = TRUE), method = 1))
+  parents <- which(abs(max(D)-D) < 1e-14, TRUE)[1,]
+  out <- which(as.integer(pop(x)) %in% parents)
+  out
 }
 
 
@@ -453,13 +456,68 @@ snapclust <- function(x, k, pop.ini = "ward", max.iter = 100, n.start = 10,
 ## returned by 'locFac([a genind])'
 
 .tidy.pop.freq <- function(pop.freq, loc.fac) {
-    g <- function(f, a = .01) {
-        (a + f) / sum(a + f)
-    }
+  g <- function(f, a = .01) {
+    (a + f) / sum(a + f)
+  }
 
-    out <- matrix(unlist(apply(pop.freq, 1, tapply, loc.fac, g),
-                          use.names = FALSE),
-                  byrow=TRUE, nrow=nrow(pop.freq))
-    dimnames(out) <- dimnames(pop.freq)
-    return(out)
+  out <- matrix(unlist(apply(pop.freq, 1, tapply, loc.fac, g),
+                       use.names = FALSE),
+                byrow=TRUE, nrow=nrow(pop.freq))
+  dimnames(out) <- dimnames(pop.freq)
+  return(out)
+}
+
+
+
+## This function rescales log-likelihood values prior to the computation of
+## group membership probabilities.
+
+## issue reported: prop.table(t(exp(ll.mat)), 1) can cause some numerical
+## approximation problems; if numbers are large, exp(...) will return Inf
+## and the group membership probabilities cannot be computed
+##
+## Solution: rather than use p_a = exp(ll_a) / (exp(ll_a) + exp(ll_b))
+## we can use p_a = exp(ll_a - C) /  (exp(ll_a - C) + exp(ll_b - C))
+
+## where 'C' is a sufficiently large constant so that exp(ll_i + C) is
+## computable; naively we could use C = max(ll.mat), but the problem is this
+## scaling can cause -Inf likelihoods too. In practice, we need to allow
+## different scaling for each individual.
+
+##out$proba <-
+## prop.table(t(exp(ll.mat)), 1)
+
+
+.rescale.ll.mat <- function(ll.mat) {
+  ## we first compute ad-hoc minimum and maximum values of log-likelihood; these
+  ## will be computer dependent; this is a quick fix, but better alternatives
+  ## can be found.
+
+  ## smallest ll such that exp(ll) is strictly > 0
+  new_min <- (0:-1000)[max(which(exp(0:-1000) > 0))]
+
+  ## largest ll such that exp(ll) is strictly less than +Inf
+  new_max <- (1:1000)[max(which(exp(1:1000) < Inf))]
+
+
+
+  ## find rescaling for a single individual;
+  ## x: vector of ll values
+  rescale.ll.indiv <- function(x) {
+    ## set minimum to new_min
+    x  <- x - min(x) + new_min
+
+    ## set sum to the maximum
+    if (sum(x) > new_max) {
+      msg <- paste("Range of log-likelihoods exceeds computer precision;",
+                   "using group membership probability approximation")
+      message(msg)
+      x <- x - min(x) # reset min to 0
+      x <- new_min + (x / sum(x)) * new_max # range: new_min to new_max
+      return(x)
+    }
+  }
+
+  out <- apply(ll.mat, 2, rescale.ll.indiv)
+
 }
