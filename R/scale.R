6771a504ce6eabf23cc638550c28cf8ff290f7f4
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-02-16 18:05:57 +0100

    Fix broken example for scaleGen #254

--- R/scale.R
+++ R/scale.R
@@ -37,32 +37,38 @@
 #' data(microbov)
 #' obj <- genind2genpop(microbov)
 #'
-#' ## compare different scaling
+#' ## apply scaling
 #' X1 <- scaleGen(obj)
-#' X2 <- scaleGen(obj,met="bin")
 #'
-#' ## compute PCAs
-#' pcaObj <- dudi.pca(obj,scale=FALSE,scannf=FALSE) # pca with no scaling
-#' pcaX1 <- dudi.pca(X1,scale=FALSE,scannf=FALSE,nf=100) # pca with usual scaling
-#' pcaX2 <- dudi.pca(X2,scale=FALSE,scannf=FALSE,nf=100) # pca with scaling for binomial variance
+#' ## compute PCAs with and without scaling
+#' pcaObj <- dudi.pca(obj, scale = FALSE, scannf = FALSE) # pca with no scaling
+#' pcaX1  <- dudi.pca(X1, scale = FALSE, scannf = FALSE, nf = 100) # pca scaled using scaleGen()
+#' pcaX2  <- dudi.pca(obj, scale = TRUE, scannf = FALSE, nf = 100) # pca scaled in-PCA
 #'
 #' ## get the loadings of alleles for the two scalings
-#' U1 <- pcaX1$c1
-#' U2 <- pcaX2$c1
-#'
+#' U1 <- pcaObj$c1
+#' U2 <- pcaX1$c1
+#' U3 <- pcaX2$c1
 #'
 #' ## find an optimal plane to compare loadings
 #' ## use a procustean rotation of loadings tables
-#' pro1 <- procuste(U1,U2,nf=2)
+#' pro1 <- procuste(U1, U2, nf = 2)
+#' pro2 <- procuste(U2, U3, nf = 2)
+#' pro3 <- procuste(U1, U3, nf = 2)
 #'
 #' ## graphics
-#' par(mfrow=c(2,2))
+#' par(mfrow=c(2, 3))
 #' # eigenvalues
-#' barplot(pcaObj$eig,main="Eigenvalues\n no scaling")
-#' barplot(pcaX1$eig,main="Eigenvalues\n usual scaling")
-#' barplot(pcaX2$eig,main="Eigenvalues\n 'binomial' scaling")
+#' barplot(pcaObj$eig, main = "Eigenvalues\n no scaling")
+#' barplot(pcaX1$eig, main = "Eigenvalues\n scaleGen scaling")
+#' barplot(pcaX2$eig, main = "Eigenvalues\n in-PCA scaling")
 #' # differences between loadings of alleles
-#' s.match(pro1$scor1,pro1$scor2,clab=0,sub="usual -> binom (procustean rotation)")
+#' s.match(pro1$scorX, pro1$scorY, clab = 0,
+#'         sub = "no scaling -> scaling (procustean rotation)")
+#' s.match(pro2$scorX, pro2$scorY, clab = 0,
+#'         sub = "scaling scaleGen -> in-PCA scaling")
+#' s.match(pro3$scorX, pro3$scorY, clab = 0,
+#'         sub = "no scaling -> in-PCA scaling")
 #'
 #' }
 #'
